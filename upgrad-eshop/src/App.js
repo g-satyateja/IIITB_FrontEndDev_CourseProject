f05d83963ef26d220d1afff3072423a342f222ed
Satya Teja Goda <g.satyateja@gmail.com>
2023-08-12 19:40:43 +0530

    SearchBar Enhancements

--- upgrad-eshop/src/App.js
+++ upgrad-eshop/src/App.js
@@ -1,18 +1,15 @@
 import { MuiNavBar } from "./components/NavBar/MuiNavBar";
 import Login from "./components/Login/Login";
 import SignUp from "./components/Register/SignUp";
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import Home from "./common/Home/Home";
-
-
 
 function App() {
   return (
     <div className="App">
-      
       <MuiNavBar />
       <Routes>
-        <Route path="/" element ={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         {/* <Route path="/products" component={<Products />} /> */}
--- upgrad-eshop/src/components/NavBar/MuiNavBar.jsx
+++ upgrad-eshop/src/components/NavBar/MuiNavBar.jsx
@@ -8,41 +8,106 @@ import {
   Typography,
   Stack,
   StyledEngineProvider,
-  TextField,
-  InputAdornment,
+  styled,
+  alpha,
+  Box,
+  InputBase,
 } from "@mui/material";
 import ShoppingCart from "@mui/icons-material/ShoppingCart";
 import SearchIcon from "@mui/icons-material/Search";
-import { Link } from "react-router-dom";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "40ch",
+    },
+  },
+}));
 
 export const MuiNavBar = () => {
   return (
-    <AppBar position="static" sx={{ alignContent: "center" }}>
-      <Toolbar sx={{ alignContent: "center" }}>
-        <IconButton size="large" edge="start" color="inherit" aria-label="logo">
+    <AppBar
+      position="static"
+      sx={{
+        justifyContent: "center",
+        backgroundColor: "#3f51b5",
+      }}
+    >
+      <Toolbar
+        variant="dense"
+        sx={{ display: "flex", justifyContent: "center" }}
+      >
+        <Stack></Stack>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="logo"
+          href="/home"
+        >
           <ShoppingCart />
         </IconButton>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           upGrad E-Shop
         </Typography>
-        <TextField
-          sx={{ flexGrow: 2 }}
-          placeholder="Search..."
-          variant="filled"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon />
-              </InputAdornment>
-            ),
-          }}
-        />
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Search…"
+            inputProps={{ "aria-label": "search" }}
+          />
+        </Search>
+        <Box sx={{ flexGrow: 1 }} />
         <Stack direction={"row"}>
           <StyledEngineProvider injectFirst>
-            <Button className="muiButton" variant="text" color="inherit" href='/login'>
+            <Button
+              className="muiButton"
+              variant="text"
+              color="inherit"
+              href="/login"
+            >
               Login
             </Button>
-            <Button className="muiButton" variant="text" color="inherit" href ='/signup'>
+            <Button
+              className="muiButton"
+              variant="text"
+              color="inherit"
+              href="/signup"
+            >
               Sign Up
             </Button>
           </StyledEngineProvider>
